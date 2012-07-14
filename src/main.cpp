7b8d819b7360f3c0199c8bb1c0656c1a3850a24a
Clarence Chuahuico <cchuahuico@gmail.com>
2012-07-13 17:27:53 -0700

    Implemented main

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,17 @@ StringTable stringtable;
 std::shared_ptr<Program> ast_root;
 
 extern int yyparse();
+extern int yynerrs;
 
 int main(int argc, char **argv)
 {
     yyparse();
+
+    if (yynerrs > 0)
+    {
+        std::cerr << argv[0] << ": **compilation halted due to lexical or syntax errors.**\n";
+        exit(1);
+    }
     
     AstNodePrintVisitor print(std::cout);
     ast_root->accept(print);
