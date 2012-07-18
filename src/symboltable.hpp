00e2607aae528f8968a5875557b3ababfd71ed34
Clarence Chuahuico <cchuahuico@gmail.com>
2012-07-18 16:05:29 -0700

    Override (in)equality operator for Symbol type

--- src/symboltable.hpp
+++ src/symboltable.hpp
@@ -18,4 +18,14 @@ inline std::ostream& operator<<(std::ostream& os, const Symbol& sym)
     return os << sym.get_val();
 }
 
+inline bool operator==(const Symbol& s1, const Symbol& s2)
+{
+    return s1.get_val() == s2.get_val();
+}
+
+inline bool operator!=(const Symbol& s1, const Symbol& s2)
+{
+    return !(s1.get_val() == s2.get_val());
+}
+
 #endif
