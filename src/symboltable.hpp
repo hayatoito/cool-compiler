2a2df602f7516ad85c52ec2460131fff31382668
Clarence Chuahuico <cchuahuico@gmail.com>
2012-07-19 19:34:30 -0700

    Added symbol table implementation

--- src/symboltable.hpp
+++ src/symboltable.hpp
@@ -2,6 +2,8 @@
 #define SYMBOLTABLE_H
 
 #include <string>
+#include <vector>
+#include <map>
 
 class Symbol 
 {
@@ -27,5 +29,47 @@ inline bool operator!=(const Symbol& s1, const Symbol& s2)
 {
     return !(s1.get_val() == s2.get_val());
 }
+
+
+template<typename K, typename V>
+class SymbolTable
+{
+private:
+    typename std::vector<std::map<K, V>> tbl;
+
+public:
+    void enter_scope()
+    {
+        tbl.push_back(std::map<K, V>());
+    }
+
+    void exit_scope()
+    {
+        tbl.pop_back();
+    }
+
+    void add(K key, V val)
+    {
+        tbl.back()[key] = val;
+    }
+
+    V probe(K key)
+    {
+        auto last = tbl.back();
+        if (last.count(key) > 0)
+            return last[key];
+        else
+            throw("key does not exist");
+    }
+
+    V lookup(K key)
+    {
+        for (auto it = tbl.rbegin(); it != tbl.rend(); ++it)
+            if (it->count(key) > 0)
+                return (*it)[key];
+
+        throw("key does not exist");
+    }
+};
 
 #endif
