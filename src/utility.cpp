c797cd566b8bc40360eae35ac59e12b23c92f8c2
Clarence Chuahuico <cchuahuico@gmail.com>
2012-08-16 17:29:07 -0700

    Added error printing functions

--- src/utility.cpp
+++ src/utility.cpp
@@ -10,4 +10,19 @@ namespace utility
         return class_sym == OBJECT || class_sym == IO || class_sym == INTEGER ||
             class_sym == BOOLEAN || class_sym == STRING;
     }
+
+    void print_error(const AstNodePtr& ast, const std::string& msg)
+    {
+        print_error(ast->filename, ast->line_no, msg);
+    }
+
+    void print_error(const std::string& filename, std::size_t line, const std::string& msg)
+    {
+        std::cerr << filename << ":" << line << ": error: " << msg << "\n";
+    }
+
+    void print_error(const std::string& filename, const std::string& msg)
+    {
+        std::cerr << filename << ": error: " << msg << "\n";
+    }
 }
--- src/utility.hpp
+++ src/utility.hpp
@@ -2,10 +2,14 @@
 #define UTILITY_H
 
 #include "symboltable.hpp"
+#include "ast.hpp"
 
 namespace utility
 {
     bool is_basic_class(const Symbol&);
+    void print_error(const AstNodePtr&, const std::string&);
+    void print_error(const std::string&, std::size_t, const std::string&);
+    void print_error(const std::string&, const std::string&);
 }
 
 #endif
